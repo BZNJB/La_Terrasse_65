5530c38b8b89820c89de98aed87df12284048b32
bznjb <bznjamin.gillens@gmail.com>
2026-08-14 12:14:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,826 @@
+const translations = {
+
+    /* =====================================================
+       FRANÇAIS
+    ===================================================== */
+
+    fr: {
+
+        restaurant: "Restaurant • Bar • Lourdes",
+
+        starters: "Entrées",
+
+        gizzard_name: "Salade de gésiers",
+        gizzard_description:
+            "Salade verte, gésiers de canard, croûtons, tomate cerise, oignon rouge, noix, vinaigrette à la moutarde à l'ancienne.",
+
+        salmon_bagel_name: "Bagel de saumon fumé",
+        salmon_bagel_description:
+            "Pain en forme de bagel, saumon fumé, salade, tomate, fromage frais aux fines herbes, concombre.",
+
+        caesar_starter_name: "Salade César",
+        caesar_starter_description:
+            "Salade, tomate cerise, poulet pané, croûtons, oignon crispy, copeaux de parmesan, sauce César.",
+
+
+        charcuterie: "Planche de charcuterie",
+        charcuterie_name: "Planche à partager",
+        charcuterie_description:
+            "Jambon de Bayonne, rosette, coppa, pâté de campagne, cornichons, chorizo, fromage et pain tranché.",
+
+
+        main_courses: "Plats",
+
+        duck_confit_name: "Confit de canard",
+
+        whole_duck_breast_name:
+            "Magret de canard entier",
+
+        half_duck_breast_name:
+            "Demi magret de canard",
+
+        salmon_name:
+            "Pavé de saumon",
+
+        salmon_description:
+            "Sauce échalote citronnée.",
+
+        caesar_main_name:
+            "Salade César",
+
+        caesar_main_description:
+            "Salade, tomate cerise, poulet pané, oignons crispy, croûtons, copeaux de parmesan.",
+
+        burger_name:
+            "Hamburger du Chef",
+
+        burger_description:
+            "Pain aux graines de sésame, salade, tomate, compotée d'oignons, tranche de bacon, emmental, steak haché de bœuf, sauce barbecue.",
+
+        fish_chips_name:
+            "Fish & Chips",
+
+        fish_chips_description:
+            "Sauce tartare.",
+
+        butcher_name:
+            "Pièce du boucher",
+
+        butcher_description:
+            "En fonction de l'arrivage.",
+
+
+        sides_title:
+            "Accompagnements au choix",
+
+        side_fries:
+            "Frites",
+
+        side_tagliatelle:
+            "Tagliatelles",
+
+        side_rice:
+            "Riz",
+
+
+        children_menu_title:
+            "Menu Enfant",
+
+        children_steak:
+            "Steak haché",
+
+        children_tenders:
+            "Tenders",
+
+        children_fish:
+            "Fish & Chips",
+
+        children_side_title:
+            "Accompagnement au choix",
+
+        children_side_description:
+            "Frites, tagliatelles ou riz",
+
+        children_drink_title:
+            "+ 1 boisson au choix",
+
+        children_drink_description:
+            "1 sirop au choix : grenadine, menthe, pêche, etc. ou Caprisun",
+
+        children_icecream_title:
+            "+ 1 boule de glace",
+
+        children_icecream_description:
+            "Parfum au choix",
+
+
+        supplements_title:
+            "Suppléments",
+
+        extra_sauce:
+            "Sauce supplémentaire",
+
+        extra_fries:
+            "Portion de frites",
+
+        extra_rice:
+            "Riz",
+
+        extra_tagliatelle:
+            "Tagliatelles",
+
+
+        desserts:
+            "Desserts",
+
+        dessert_name:
+            "Dessert au choix",
+
+        dessert_description:
+            "En fonction de l'arrivage.",
+
+
+        ice_creams:
+            "Glaces",
+
+        one_scoop:
+            "1 boule",
+
+        two_scoops:
+            "2 boules",
+
+        icecream_cup:
+            "Coupe glacée",
+
+        icecream_cup_description:
+            "3 boules + chantilly + sirop",
+
+        extra_whipped_cream:
+            "Supplément chantilly",
+
+
+        footer_restaurant:
+            "La Terrasse • Lourdes",
+
+        footer_thanks:
+            "Merci de votre visite ❤️"
+    },
+
+
+    /* =====================================================
+       ENGLISH
+    ===================================================== */
+
+    en: {
+
+        restaurant:
+            "Restaurant • Bar • Lourdes",
+
+        starters:
+            "Starters",
+
+        gizzard_name:
+            "Duck Gizzard Salad",
+
+        gizzard_description:
+            "Green salad, duck gizzards, croutons, cherry tomato, red onion, walnuts and wholegrain mustard dressing.",
+
+        salmon_bagel_name:
+            "Smoked Salmon Bagel",
+
+        salmon_bagel_description:
+            "Bagel, smoked salmon, lettuce, tomato, fresh herb cream cheese and cucumber.",
+
+        caesar_starter_name:
+            "Caesar Salad",
+
+        caesar_starter_description:
+            "Salad, cherry tomato, breaded chicken, croutons, crispy onions, Parmesan shavings and Caesar dressing.",
+
+
+        charcuterie:
+            "Charcuterie Board",
+
+        charcuterie_name:
+            "Sharing Board",
+
+        charcuterie_description:
+            "Bayonne ham, dry-cured sausage, coppa, country pâté, pickles, chorizo, cheese and sliced bread.",
+
+
+        main_courses:
+            "Main Courses",
+
+        duck_confit_name:
+            "Duck Confit",
+
+        whole_duck_breast_name:
+            "Whole Duck Breast",
+
+        half_duck_breast_name:
+            "Half Duck Breast",
+
+        salmon_name:
+            "Salmon Fillet",
+
+        salmon_description:
+            "Lemon and shallot sauce.",
+
+        caesar_main_name:
+            "Caesar Salad",
+
+        caesar_main_description:
+            "Salad, cherry tomato, breaded chicken, crispy onions, croutons and Parmesan shavings.",
+
+        burger_name:
+            "Chef's Burger",
+
+        burger_description:
+            "Sesame seed bun, lettuce, tomato, onion confit, bacon, Emmental cheese, beef patty and barbecue sauce.",
+
+        fish_chips_name:
+            "Fish & Chips",
+
+        fish_chips_description:
+            "Tartar sauce.",
+
+        butcher_name:
+            "Butcher's Cut",
+
+        butcher_description:
+            "Subject to availability.",
+
+
+        sides_title:
+            "Choice of side",
+
+        side_fries:
+            "French fries",
+
+        side_tagliatelle:
+            "Tagliatelle",
+
+        side_rice:
+            "Rice",
+
+
+        children_menu_title:
+            "Kids' Menu",
+
+        children_steak:
+            "Beef patty",
+
+        children_tenders:
+            "Chicken tenders",
+
+        children_fish:
+            "Fish & Chips",
+
+        children_side_title:
+            "Choice of side",
+
+        children_side_description:
+            "French fries, tagliatelle or rice",
+
+        children_drink_title:
+            "+ 1 drink of your choice",
+
+        children_drink_description:
+            "1 syrup drink of your choice: grenadine, mint, peach, etc. or Caprisun",
+
+        children_icecream_title:
+            "+ 1 scoop of ice cream",
+
+        children_icecream_description:
+            "Flavour of your choice",
+
+
+        supplements_title:
+            "Extras",
+
+        extra_sauce:
+            "Extra sauce",
+
+        extra_fries:
+            "Portion of French fries",
+
+        extra_rice:
+            "Rice",
+
+        extra_tagliatelle:
+            "Tagliatelle",
+
+
+        desserts:
+            "Desserts",
+
+        dessert_name:
+            "Dessert of your choice",
+
+        dessert_description:
+            "Subject to availability.",
+
+
+        ice_creams:
+            "Ice Cream",
+
+        one_scoop:
+            "1 scoop",
+
+        two_scoops:
+            "2 scoops",
+
+        icecream_cup:
+            "Ice Cream Sundae",
+
+        icecream_cup_description:
+            "3 scoops + whipped cream + syrup",
+
+        extra_whipped_cream:
+            "Extra whipped cream",
+
+
+        footer_restaurant:
+            "La Terrasse • Lourdes",
+
+        footer_thanks:
+            "Thank you for your visit ❤️"
+    },
+
+
+    /* =====================================================
+       ESPAÑOL
+    ===================================================== */
+
+    es: {
+
+        restaurant:
+            "Restaurante • Bar • Lourdes",
+
+        starters:
+            "Entrantes",
+
+        gizzard_name:
+            "Ensalada de mollejas de pato",
+
+        gizzard_description:
+            "Ensalada verde, mollejas de pato, picatostes, tomate cherry, cebolla roja, nueces y vinagreta de mostaza a la antigua.",
+
+        salmon_bagel_name:
+            "Bagel de salmón ahumado",
+
+        salmon_bagel_description:
+            "Pan de bagel, salmón ahumado, ensalada, tomate, queso fresco con finas hierbas y pepino.",
+
+        caesar_starter_name:
+            "Ensalada César",
+
+        caesar_starter_description:
+            "Ensalada, tomate cherry, pollo empanado, picatostes, cebolla crujiente, virutas de parmesano y salsa César.",
+
+
+        charcuterie:
+            "Tabla de embutidos",
+
+        charcuterie_name:
+            "Tabla para compartir",
+
+        charcuterie_description:
+            "Jamón de Bayona, salchichón, coppa, paté de campaña, pepinillos, chorizo, queso y pan en rebanadas.",
+
+
+        main_courses:
+            "Platos",
+
+        duck_confit_name:
+            "Confit de pato",
+
+        whole_duck_breast_name:
+            "Magret de pato entero",
+
+        half_duck_breast_name:
+            "Medio magret de pato",
+
+        salmon_name:
+            "Lomo de salmón",
+
+        salmon_description:
+            "Salsa de chalota y limón.",
+
+        caesar_main_name:
+            "Ensalada César",
+
+        caesar_main_description:
+            "Ensalada, tomate cherry, pollo empanado, cebolla crujiente, picatostes y virutas de parmesano.",
+
+        burger_name:
+            "Hamburguesa del Chef",
+
+        burger_description:
+            "Pan con semillas de sésamo, lechuga, tomate, cebolla confitada, bacon, queso Emmental, carne picada de vacuno y salsa barbacoa.",
+
+        fish_chips_name:
+            "Fish & Chips",
+
+        fish_chips_description:
+            "Salsa tártara.",
+
+        butcher_name:
+            "Pieza del carnicero",
+
+        butcher_description:
+            "Según disponibilidad.",
+
+
+        sides_title:
+            "Guarnición a elegir",
+
+        side_fries:
+            "Patatas fritas",
+
+        side_tagliatelle:
+            "Tagliatelle",
+
+        side_rice:
+            "Arroz",
+
+
+        children_menu_title:
+            "Menú Infantil",
+
+        children_steak:
+            "Carne picada de vacuno",
+
+        children_tenders:
+            "Tenders de pollo",
+
+        children_fish:
+            "Fish & Chips",
+
+        children_side_title:
+            "Guarnición a elegir",
+
+        children_side_description:
+            "Patatas fritas, tagliatelle o arroz",
+
+        children_drink_title:
+            "+ 1 bebida a elegir",
+
+        children_drink_description:
+            "1 sirope a elegir: granadina, menta, melocotón, etc. o Caprisun",
+
+        children_icecream_title:
+            "+ 1 bola de helado",
+
+        children_icecream_description:
+            "Sabor a elegir",
+
+
+        supplements_title:
+            "Suplementos",
+
+        extra_sauce:
+            "Salsa extra",
+
+        extra_fries:
+            "Porción de patatas fritas",
+
+        extra_rice:
+            "Arroz",
+
+        extra_tagliatelle:
+            "Tagliatelle",
+
+
+        desserts:
+            "Postres",
+
+        dessert_name:
+            "Postre a elegir",
+
+        dessert_description:
+            "Según disponibilidad.",
+
+
+        ice_creams:
+            "Helados",
+
+        one_scoop:
+            "1 bola",
+
+        two_scoops:
+            "2 bolas",
+
+        icecream_cup:
+            "Copa de helado",
+
+        icecream_cup_description:
+            "3 bolas + nata montada + sirope",
+
+        extra_whipped_cream:
+            "Suplemento de nata montada",
+
+
+        footer_restaurant:
+            "La Terrasse • Lourdes",
+
+        footer_thanks:
+            "Gracias por su visita ❤️"
+    },
+
+
+    /* =====================================================
+       ITALIANO
+    ===================================================== */
+
+    it: {
+
+        restaurant:
+            "Ristorante • Bar • Lourdes",
+
+        starters:
+            "Antipasti",
+
+        gizzard_name:
+            "Insalata di ventrigli d'anatra",
+
+        gizzard_description:
+            "Insalata verde, ventrigli d'anatra, crostini, pomodorino, cipolla rossa, noci e vinaigrette alla senape all'antica.",
+
+        salmon_bagel_name:
+            "Bagel al salmone affumicato",
+
+        salmon_bagel_description:
+            "Pane bagel, salmone affumicato, insalata, pomodoro, formaggio fresco alle erbe e cetriolo.",
+
+        caesar_starter_name:
+            "Insalata Caesar",
+
+        caesar_starter_description:
+            "Insalata, pomodorino, pollo impanato, crostini, cipolla croccante, scaglie di parmigiano e salsa Caesar.",
+
+
+        charcuterie:
+            "Tagliere di salumi",
+
+        charcuterie_name:
+            "Tagliere da condividere",
+
+        charcuterie_description:
+            "Prosciutto di Bayonne, salame, coppa, pâté rustico, cetriolini, chorizo, formaggio e pane a fette.",
+
+
+        main_courses:
+            "Piatti",
+
+        duck_confit_name:
+            "Confit d'anatra",
+
+        whole_duck_breast_name:
+            "Petto d'anatra intero",
+
+        half_duck_breast_name:
+            "Mezzo petto d'anatra",
+
+        salmon_name:
+            "Filetto di salmone",
+
+        salmon_description:
+            "Salsa allo scalogno e limone.",
+
+        caesar_main_name:
+            "Insalata Caesar",
+
+        caesar_main_description:
+            "Insalata, pomodorino, pollo impanato, cipolla croccante, crostini e scaglie di parmigiano.",
+
+        burger_name:
+            "Hamburger dello Chef",
+
+        burger_description:
+            "Pane con semi di sesamo, insalata, pomodoro, cipolla confit, bacon, formaggio Emmental, hamburger di manzo e salsa barbecue.",
+
+        fish_chips_name:
+            "Fish & Chips",
+
+        fish_chips_description:
+            "Salsa tartara.",
+
+        butcher_name:
+            "Taglio del macellaio",
+
+        butcher_description:
+            "Secondo disponibilità.",
+
+
+        sides_title:
+            "Contorno a scelta",
+
+        side_fries:
+            "Patatine fritte",
+
+        side_tagliatelle:
+            "Tagliatelle",
+
+        side_rice:
+            "Riso",
+
+
+        children_menu_title:
+            "Menu Bambini",
+
+        children_steak:
+            "Hamburger di manzo",
+
+        children_tenders:
+            "Tenders di pollo",
+
+        children_fish:
+            "Fish & Chips",
+
+        children_side_title:
+            "Contorno a scelta",
+
+        children_side_description:
+            "Patatine fritte, tagliatelle o riso",
+
+        children_drink_title:
+            "+ 1 bevanda a scelta",
+
+        children_drink_description:
+            "1 sciroppo a scelta: granatina, menta, pesca, ecc. oppure Caprisun",
+
+        children_icecream_title:
+            "+ 1 pallina di gelato",
+
+        children_icecream_description:
+            "Gusto a scelta",
+
+
+        supplements_title:
+            "Supplementi",
+
+        extra_sauce:
+            "Salsa extra",
+
+        extra_fries:
+            "Porzione di patatine fritte",
+
+        extra_rice:
+            "Riso",
+
+        extra_tagliatelle:
+            "Tagliatelle",
+
+
+        desserts:
+            "Dessert",
+
+        dessert_name:
+            "Dessert a scelta",
+
+        dessert_description:
+            "Secondo disponibilità.",
+
+
+        ice_creams:
+            "Gelati",
+
+        one_scoop:
+            "1 pallina",
+
+        two_scoops:
+            "2 palline",
+
+        icecream_cup:
+            "Coppa gelato",
+
+        icecream_cup_description:
+            "3 palline + panna montata + sciroppo",
+
+        extra_whipped_cream:
+            "Supplemento panna montata",
+
+
+        footer_restaurant:
+            "La Terrasse • Lourdes",
+
+        footer_thanks:
+            "Grazie per la vostra visita ❤️"
+    }
+
+};
+
+
+/* =========================================================
+   BOUTONS DE LANGUE
+========================================================= */
+
+const languageButtons =
+    document.querySelectorAll(".language-btn");
+
+
+languageButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const language =
+            button.dataset.language;
+
+        changeLanguage(language);
+
+    });
+
+});
+
+
+/* =========================================================
+   CHANGEMENT DE LANGUE
+========================================================= */
+
+function changeLanguage(language) {
+
+    const selectedTranslations =
+        translations[language];
+
+    if (!selectedTranslations) {
+        return;
+    }
+
+
+    document
+        .querySelectorAll("[data-i18n]")
+        .forEach(element => {
+
+            const key =
+                element.dataset.i18n;
+
+            if (
+                Object.prototype.hasOwnProperty.call(
+                    selectedTranslations,
+                    key
+                )
+            ) {
+
+                element.textContent =
+                    selectedTranslations[key];
+
+            }
+
+        });
+
+
+    /*
+     * Met à jour l'attribut lang du HTML.
+     */
+
+    document.documentElement.lang =
+        language;
+
+
+    /*
+     * Met à jour le bouton sélectionné.
+     */
+
+    languageButtons.forEach(button => {
+
+        button.classList.toggle(
+            "active",
+            button.dataset.language === language
+        );
+
+    });
+
+
+    /*
+     * Mémorise la langue choisie.
+     */
+
+    localStorage.setItem(
+        "preferredLanguage",
+        language
+    );
+
+}
+
+
+/* =========================================================
+   LANGUE MÉMORISÉE
+========================================================= */
+
+const savedLanguage =
+    localStorage.getItem("preferredLanguage");
+
+
+if (
+    savedLanguage &&
+    translations[savedLanguage]
+) {
+
+    changeLanguage(savedLanguage);
+
+} else {
+
+    changeLanguage("fr");
+
+}
